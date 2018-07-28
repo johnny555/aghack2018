9994da93170cd0a613c4399794b41ca45a6716f5
John Vial <badwolf.johnnyv@gmail.com>
2018-07-28 22:03:17 +0800

    continuing to use JS as a dodgy hack... will regret this later

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -6,15 +6,40 @@ module Frontend where
 import qualified Data.Text as T
 import Reflex.Dom.Core
 
+import Reflex.Dom.SemanticUI
+import qualified Reflex.Dom.SemanticUI as S
+
+
+import Control.Lens
+
 import Common.Api
 import Static
-
+import Data.Map (fromList)
 import Data.Monoid ((<>))
+
+import           Language.Javascript.JSaddle.Types  (liftJSM, JSString, JSVal, JSM, MonadJSM)
+
+import           Language.Javascript.JSaddle        (jss, js0, js1, js2,  jsg, create, setProp, textToJSString, toJSVal)
+
+
+import Data.ByteString
 
 frontend :: (StaticWidget x (), Widget x ())
 frontend = (head', body)
   where
-    head' = el "title" $ text "Bugadex Minimal Example"
+    head' = do
+      el "title" $ text "Bugadex Minimal Example"
+      styleSheet "styling.css"
+      styleSheet "semantic.min.css"
+      styleSheet "extra.css"
+
+      where
+        styleSheet linkvar = elAttr "link" (fromList [
+                                              ("rel","stylesheet")
+                                            , ("type", "text/css")
+                                            , ("href", linkvar)
+                                            ]) $ return ()
+
     body= bod
 
 -- | This loads the JS libs, and then returns an event that fires when they have all loaded.
@@ -22,21 +47,71 @@ loadJSLibs :: MonadWidget t m => m (Event t ())
 loadJSLibs = do
   let
       custSrc = "customVis.js"
-
+      awsSrc = "aws-sdk-2.282.1.min.js"
   (cust_script, _) <- elAttr' "script" ("src" =: custSrc) blank
+  (aws_script, _) <- elAttr' "script" ("src" =: awsSrc) blank
 
-  pure $ () <$ domEvent Load cust_script
+  let
+      addOne a = a + 1
+      addOne :: Integer -> Integer
+      awsLoaded = (addOne) <$ domEvent Load aws_script
+      custScriptLoaded = (addOne) <$ domEvent Load cust_script
+      eventList = [awsLoaded,  custScriptLoaded]
+      lenEventList = Prelude.length eventList
+  numberLoaded <- foldDyn ($) 0 $ mergeWith (.) eventList
 
+  let ready = ffilter (==(fromIntegral lenEventList)) $ updated numberLoaded
+
+  pure $ () <$ ready
 
 
 bod :: MonadWidget t m => m ()
 bod = do
+  container def $ do
+    pageHeader H1 def $ text "Welcome to BugADex!"
+    subHeader  $ text $ "Take a photo of a bug!"
+    let dimensions ="width"=:"640"<>"height"=:"480"
+    (click, res) <- divClass "ui grid" $ do
+      divClass "ui row" $ elAttr "video" ("id"=:"video"<>dimensions <> "autoplay"=:"") $ blank
+      c <- divClass "ui row " $ do
+        c <- S.button def $ text "Snap Photo"
+        r <- S.button def $ text "Result "
+        pure (c, r)
+      divClass "ui row" $ elAttr "canvas" (dimensions <> "id"=:"canvas") $ text "Please wait untill video is ready..."
+      segment def $ text "no results yet"
+      pure c
 
-  text "Welcome to BugADex!"
-  el "p" $ text $ "Take a photo of a bug!"
-  let dimensions ="width"=:"640"<>"height"=:"480"
-  elAttr "video" ("id"=:"video"<>dimensions <> "autoplay"=:"") blank
-  elAttr "button" ("id"=:"snap") $ text "Snap Photo"
-  elAttr "canvas" (dimensions <> "id"=:"canvas") blank
-  jsReady <- loadJSLibs
-  blank
+    jsReady <- loadJSLibs
+
+    up <- delay 0.5 click
+    widgetHold blank $ (const takePhoto) <$> click
+    widgetHold blank $ (const uploadPhoto) <$> up
+    widgetHold blank $ (const runResult) <$> res
+    blank
+
+
+
+takePhoto :: (MonadWidget t m, MonadJSM m) =>  m ()
+takePhoto = do
+  liftJSM $ do
+    photoTaker <- jsg ("photo_settings" :: String)
+
+    photoTaker ^. js2 ("takepicture" :: String) ("640" :: String ) ("480" :: String)
+    uploader <- jsg ("uploader" :: String)
+    uploader ^. js0 ("init" :: String)
+  pure ()
+
+uploadPhoto :: (MonadWidget t m, MonadJSM m) =>  m ()
+uploadPhoto = do
+  liftJSM $ do
+    uploader <- jsg ("uploader" :: String)
+    uploader ^. js0 ("upload" :: String)
+  pure ()
+
+
+runResult :: (MonadWidget t m, MonadJSM m) =>  m ()
+runResult = do
+  liftJSM $ do
+    resulter <- jsg ("resulter" :: String)
+    resulter ^. js0 ("getResult" :: String)
+  pure ()
--- static/customVis.js
+++ static/customVis.js
@@ -1,14 +1,15 @@
 var video = document.getElementById('video');
+//var photo = document.getElementById('photo');
 
 
 
 function cameraStart() {
     console.log("starting camera...");
     navigator.mediaDevices.getUserMedia({
-    audio: false,
+    audio: true,
     video: true
 }).then(function ( stream) {
-    video.src = stream;
+    video.srcObject = stream;
     video.play();
 }).catch(function (err) {
     console.log("An error occurred! " + err);
@@ -22,9 +23,73 @@ photo_settings.takepicture = function (width, height) {
 
     var context = canvas.getContext('2d');
     context.drawImage(video, 0, 0, width, height);
+    var img = canvas.toDataURL('image/png');
+    var data = atob(img.split(',')[1]);
+    var ia = new Uint8Array(data.length);
+    for (var i = 0; i < data.length; i++) {
+        ia[i] = data.charCodeAt(i);
+    }
+    blobData = new Blob([ia], {type: "mimeString"});
 
-    var data = canvas.toDataURL('image/png');
-    return data;
+    canvas.getContext('2d').drawImage(video, 0,0,width,height);
+    photo_settings.img = blobData;
+    return blobData;
+};
+
+var albumBucketName = 'dex-hackathon-bucket-2';
+var bucketRegion = 'us-east-1';
+var IdentityPoolId = 'us-east-1:71e2f4ab-2db9-4039-abd5-35c66e86937c';
+
+
+uploader = {};
+uploader.init = function () {
+
+    AWS.config.update({
+        region: bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: IdentityPoolId
+        })
+    });
+
+    var s3 = new AWS.S3({
+        apiVersion: '2006-03-01',
+        params: {Bucket: albumBucketName}
+    });
+
+    uploader.s3 = s3;
+
+};
+
+uploader.upload = function () {
+    var s3 = uploader.s3;
+    s3.upload({
+        Key:  'testfile.png',
+        Body: photo_settings.img,
+        ACL : 'public-read'
+    }, function (err, data) {
+        if (err) {
+            return alert("There was an error uploading");
+        }
+        alert("Successfully uploaded");
+    });
+};
+
+
+resulter = {};
+resulter.listener = (e) => {
+    console.log(e);
+};
+resulter.getResult = function () {
+    var oReq = new XMLHttpRequest();
+    var params = {image_url: "https://s3.amazonaws.com/dex-hackathon-bucket-2/testfile.png",
+                  top_k: "3"};
+    var queryString = "https://b88bcz9xhe.execute-api.us-east-1.amazonaws.com/dev/v0.0.1/predict" +
+        "?" + Object.keys(params).map((key) => {return key + "=" + encodeURIComponent(params[key])}).join("&");
+    oReq.open("GET",
+              queryString);
+    oReq.setRequestHeader("x-api-key", "Pc0H7UGiNL4DuShrbs1uO4DAsyQFhf5j8uLmXJI9");
+    oReq.send();
+
 };
 
 //window.addEventListener("load", cameraStart, false);
