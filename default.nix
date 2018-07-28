47818e0a1091fc6c819a0e4f8aa72494f8f020ca
John Vial <badwolf.johnnyv@gmail.com>
2018-07-28 17:54:20 +0800

    Playing with js

--- default.nix
+++ default.nix
@@ -10,7 +10,10 @@ project ./. ({pkgs,  ... }: {
                   rev = "38fce7e4d08d46b8664768f1b7fe38846dbac1e2";
                   sha256 = "1s2p12r682wd8j2z63pjvbi4s9v02crh6nz8kjilwdsfs02yp5p2";
                   };
-                  semantic-reflex = self.callCabal2nix "semantic-reflex" "${semantic-reflex}/semantic-reflex" {};
+                  in {
+  semantic-reflex = self.callCabal2nix "semantic-reflex" "${semantic-reflex}/semantic-reflex" {};
+  };
+
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
--- static/customVis.js
+++ static/customVis.js
@@ -1,8 +1,29 @@
 var video = document.getElementById('video');
-navigator.mediaDevices.getUserMedia({
+
+
+
+function cameraStart() {
+    navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true
 }).then(function ( stream) {
     video.src = stream;
     video.play();
-});;
+}).catch(function (err) {
+    console.log("An error occurred! " + err);
+});
+                       };
+
+var canvas = document.getElementById('canvas');
+
+photo_settings = {};
+photo_settings.takepicture = function (width, height) {
+
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, width, height);
+
+    var data = canvas.toDataURL('image/png');
+    return data;
+};
+
+window.addEventListener("load", cameraStart, false);
