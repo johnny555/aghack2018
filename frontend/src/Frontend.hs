e6c5a37238169d9c70fe8fdd7cc46c90000152bb
John Vial <badwolf.johnnyv@gmail.com>
2018-07-28 16:39:34 +0800

    Baby steps

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -12,8 +12,25 @@ import Static
 frontend :: (StaticWidget x (), Widget x ())
 frontend = (head', body)
   where
-    head' = el "title" $ text "Obelisk Minimal Example"
-    body = do
-      text "Welcome to Obelisk!"
-      el "p" $ text $ T.pack commonStuff
-      elAttr "img" ("src" =: static @"obelisk.jpg") blank
+    head' = el "title" $ text "Bugadex Minimal Example"
+    body= bod
+
+-- | This loads the JS libs, and then returns an event that fires when they have all loaded.
+loadJSLibs :: MonadWidget t m => m (Event t ())
+loadJSLibs = do
+  let
+      custSrc = "customVis.js"
+
+  (cust_script, _ ) <- elAttr' "script" ("src" =: custSrc) blank
+
+  pure $ () <$ domEvent Load cust_script
+
+
+
+bod :: MonadWidget t m => m ()
+bod = do
+  jsReady <- loadJSLibs
+  text "Welcome to Obelisk!"
+  el "p" $ text $ T.pack commonStuff
+  elAttr "img" ("src" =: static @"obelisk.jpg") blank
+  blank
